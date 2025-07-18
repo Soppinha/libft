48eac6e2ef852cc71476922effa894ee133e0e6d
Sofia Valadares Bastos <svaladar@c2r4p3.42sp.org.br>
2025-07-18 10:49:09 -0300

    add justes

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,30 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/17 11:05:33 by svaladar          #+#    #+#             */
-/*   Updated: 2025/07/17 11:36:35 by svaladar         ###   ########.fr       */
+/*   Created: 2025/07/17 11:12:39 by svaladar          #+#    #+#             */
+/*   Updated: 2025/07/17 11:40:49 by svaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*tmp_dst;
-	unsigned char	*tmp_src;
+	char	*tmp_dst;
+	char	*tmp_src;
+	size_t	count;
 
-	if (src == NULL && dst == NULL)
-		return (dst);
-	tmp_dst = (unsigned char *) dst;
-	tmp_src = (unsigned char *) src;
-	while (n > 0)
+	if (!src && !dst)
+		return ((void *) 0);
+	tmp_dst == (char *) dst;
+	tmp_src == (char *) src;
+	count = 0;
+	if (tmp_dst > tmp_src)
 	{
-		*(tmp_dst++) = *(tmp_src++);
-		n--;
+		while (len-- > 0)
+			tmp_dst = tmp_src;
+	}
+	else
+	{
+		while (i++ < len)
+			tmp_dst[i] = tmp_src[i];
 	}
 	return (dst);
 }
