c2bfeeadc23226a331948d33ef53e318bdae586e
Sofia Valadares Bastos <svaladar@c2r4p3.42sp.org.br>
2025-07-18 12:06:28 -0300

    add func

---
+++ ft_strlcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/18 10:52:02 by svaladar          #+#    #+#             */
+/*   Updated: 2025/07/18 12:05:26 by svaladar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	int	src_len;
+
+	src_len = ft_strlen(src);
+	if (dstsize > src_len + 1)
+		ft_memcpy(dst, src, src_len + 1);
+	else if (dstsize != 0)
+	{
+		ft_memcpy(dst, src, dstsize - 1);
+		dst[dstsize - 1] = '\0';
+	}
+	return (src_len);
+}
