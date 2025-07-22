24ac5f86fe4e0d4c44336c30720bf6fd0b0e118e
Sofia Valadares Bastos <svaladar@student.42sp.org.br>
2025-07-22 11:11:05 -0300

    finish func

---
+++ ft_strrchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 10:07:12 by svaladar          #+#    #+#             */
+/*   Updated: 2025/07/22 10:21:02 by svaladar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_strrchr(const char *s, int c)
+{	
+	int		i;
+	char	charc;
+	char	res;
+
+	i = 0;
+	charc = (char) c;
+	res = NULL;
+	while (s[i])
+	{
+		if (s[i] == charc)
+			res = (char *) &s[i];
+		i++;
+	}
+	if (s[i] == charc)
+			res = (char *) &s[i];
+	return (res);
+}
