7d83e23fb45af73466f5a678c334b608e552945f
Sofia Valadares Bastos <svaladar@student.42sp.org.br>
2025-07-22 13:47:42 -0300

    finished func

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 12:01:08 by svaladar          #+#    #+#             */
+/*   Updated: 2025/07/22 13:44:47 by svaladar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+	unsigned char	charac;
+
+	i = 0;
+	str = (unsigned char *) s;
+	charac = (unsigned char) c;
+	while (i < n)
+	{
+		if (str[i] == charac)
+			return ((void *) &str[i]);
+		i++;
+	}
+	return (NULL);
+}
