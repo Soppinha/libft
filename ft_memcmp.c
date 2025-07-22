755b317864965f991a1ef34a03b342a49ffa7b04
Sofia Valadares Bastos <svaladar@student.42sp.org.br>
2025-07-22 14:06:20 -0300

    finished the func

---
+++ ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 13:49:32 by svaladar          #+#    #+#             */
+/*   Updated: 2025/07/22 14:03:54 by svaladar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+		{
+			return ((unsigned char) str1[i] - (unsigned char) str2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
