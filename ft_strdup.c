9bc6235cace60532b299488a5224243e73b40c7b
Sofia Valadares Bastos <svaladar@student.42sp.org.br>
2025-07-25 10:27:03 -0300

    completed function

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 09:58:51 by svaladar          #+#    #+#             */
+/*   Updated: 2025/07/25 10:26:04 by svaladar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_strdup(const char *s1)
+{
+	char	*temp;
+	size_t	i;
+
+	temp = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
+	if (!temp)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		temp[i] = s1[i];
+		i++;
+	}
+	temp[i] = '\0';
+	return (temp);
+}
