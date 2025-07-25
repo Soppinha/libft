8286f12521a428f20077d63fd448bcf5edd33c5a
Sofia Valadares Bastos <svaladar@student.42sp.org.br>
2025-07-25 17:15:37 -0300

    function complete

---
+++ ft_substr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 10:51:44 by svaladar          #+#    #+#             */
+/*   Updated: 2025/07/25 11:38:05 by svaladar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*new_str;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	new_str = ft_calloc((len + 1), sizeof(char));
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		new_str[i] = s[start + i];
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
