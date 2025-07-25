3e583e28d9d6b93b27ae79d0ff141e2abab3db75
Sofia Valadares Bastos <svaladar@student.42sp.org.br>
2025-07-25 10:49:09 -0300

    ajustment norminette

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 14:21:07 by svaladar          #+#    #+#             */
-/*   Updated: 2025/07/23 13:42:09 by svaladar         ###   ########.fr       */
+/*   Updated: 2025/07/25 10:45:33 by svaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,11 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	while (i < len && haystack[i])
 	{
 		j = 0;
-		while (haystack[i + j] && needle[j] && i + j < len && haystack[i + j] == needle[j])
+		while (haystack[i + j] && needle[j] && i + j < len
+			&& haystack[i + j] == needle[j])
 			j++;
 		if (!needle[j])
-				return ((char *) haystack + i);
+			return ((char *) haystack + i);
 		i++;
 	}
 	return (NULL);
