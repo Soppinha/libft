4fccd5048a02f927e1c116a6eb99a7f37818be09
Sofia Valadares Bastos <svaladar@student.42sp.org.br>
2025-07-25 09:58:01 -0300

    completed function

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,22 @@
 /*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 09:40:46 by svaladar          #+#    #+#             */
-/*   Updated: 2025/07/25 09:40:47 by svaladar         ###   ########.fr       */
+/*   Updated: 2025/07/25 09:53:14 by svaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <libft.h>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	unsigned char	*temp;
+	size_t			i;
+
+	i = 0;
+	temp = malloc(count * size);
+	if (!temp)
+		return (NULL);
+	while (i < (count * size))
+		temp[i++] = 0;
+	return (temp);
+}
