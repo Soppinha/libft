ca6944c510d9dae227bfc2350f8841393269f017
Sofia Valadares Bastos <svaladar@student.42sp.org.br>
2025-07-25 17:17:17 -0300

    mais que função desgraçada

--- ft_split.c
+++ ft_split.c
@@ -1,31 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: svaladar <svaladar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/19 16:11:30 by svaladar          #+#    #+#             */
-/*   Updated: 2025/07/25 13:38:01 by svaladar         ###   ########.fr       */
+/*   Created: 2025/07/25 13:44:05 by svaladar          #+#    #+#             */
+/*   Updated: 2025/07/25 17:06:11 by svaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-char	*ft_strchr(const char *s, int c)
+char	**ft_split(char const *s, char c)
 {
-	int		i;
-	char	chr;
+	if (!s)
+		return (NULL);
+	
+}
+
+static size_t	count_words(char *s, char c)
+{
+	int		n_word;
+	size_t	i;
+
+	while (*s)
+	{
+		if (*s != c && n_word == 0)
+		{
+			n_word = 1;
+			i++;
+		}
+		else if (*s == c)
+			n_word = 0;
+		s++;
+	}
+	return (i);
+}
+
+static void	*ft_free(char	**ss, int count)
+{
+	int	i;
 
 	i = 0;
-	chr = (char) c;
-	while (s[i])
+	while (i < count)
 	{
-		if (s[i] == chr)
-			return ((char *) &s[i]);
+		free(ss[i]);
 		i++;
 	}
-	if (s[i] == chr)
-		return ((char *) &s[i]);
+	free(ss);
 	return (NULL);
+}
+
+static char	*new_word(char *c)
+{
+	char	*new_str;
+
+	
 }
