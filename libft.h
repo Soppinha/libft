ea6199dc0b77002360361af1b470184eeb2cd2b3
Sofia Valadares Bastos <louisafolkens@gmail.com>
2025-08-19 12:04:18 -0300

    ajustments

--- libft.h
+++ libft.h
@@ -44,7 +44,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
